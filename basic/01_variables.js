eeceb7c88648e1a236033d2120a4f2dbf4c864df
unknown <harshitajayant25@gmail.com>
2024-06-29 15:09:28 +0530

    first commit

---
+++ basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=14455
+let accountEmail="harshita@gmail.com"
+var accountPasssword="12345"
+accountCity = "jaipur"
+let accountState;
+// accountId = 2
+console.log(accountEmail);
+console.table([accountId, accountPasssword,accountCity,accountState])
+/*
+var ko kabhi use mt kriye:becase of issue in block scope and functional scope
+*/
---
+++ basic/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as a new version
+//alert(3+3) we are using ndejs not browser//
+let name="harshi"
+let age=13
+let islogged=false
+
+
+//num=>2 to power 53
+//bigint 
+//boolean => true or not//
+// string (" ")//
+//null-> standalone value//
+//undifine => //
+//symbol => for uniqueness//
+
+//objects//
+console.log(typeof"hi");
+console.log(typeof null)  //object//
+console.log(typeof undefined);
+
